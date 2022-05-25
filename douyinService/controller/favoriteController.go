7336193c3e18e51c0eeeb1f7321c2618d8512d64
ddd <3136607435@qq.com>
2022-05-25 21:07:16 +0800

    修改favoriteController接口

--- douyinService/controller/favoriteController.go
+++ douyinService/controller/favoriteController.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/DouYin/common/entity/response"
 	"github.com/DouYin/common/model"
 	"github.com/DouYin/service/service"
@@ -8,7 +9,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/google/martian/log"
 	"net/http"
-	"strconv"
 )
 
 var favoriteService service.FavoriteService
@@ -18,45 +18,49 @@ const (
 	UNLIKE bool = true
 )
 
+type QueryInfo struct {
+	UserId     int64  `form:"user_id" binding:"required"`
+	Token      string `form:"token" binding:"required"`
+	VideoId    int64  `form:"video_id" binding:"required"`
+	ActionType int32  `form:"action_type" binding:"required"`
+}
+
 // FavoriteAction
 // @Description: 登录用户对视频的点赞和取消点赞操作
 // @param: c
 func FavoriteAction(c *gin.Context) {
 	//鉴权
-	claims := jwt.ExtractClaims(c)
-	userID := uint64(claims["id"].(float64))
-	log.Infof("userID", userID)
+	//claims := jwt.ExtractClaims(c)
+	//userId := uint64(claims["id"].(float64))
+	//log.Infof("userID", userId)
 
-	var err error
-	var favoriteInfo model.Favorite
-
-	err = c.ShouldBindQuery(&favoriteInfo)
+	var queryInfo QueryInfo
+	err := c.ShouldBind(&queryInfo)
 	if err != nil {
 		log.Errorf("获取favoriteInfo失败", err)
 	}
-	//log.Infof("favoriteInfo", favoriteInfo)
-	var isDeleted int64
-	isDeleted, err = strconv.ParseInt(c.Query("action_type"), 10, 64)
-	if err != nil {
-		log.Errorf("获取action_type失败", err)
-	}
+	fmt.Println("queryInfo: ", queryInfo)
+
+	var favoriteInfo model.Favorite
 
 	//1是点赞(false),2是取消(true)
-	if isDeleted == 1 {
+
+	if queryInfo.ActionType == 1 {
 		favoriteInfo.IsDeleted = LIKE
-	} else if isDeleted == 2 {
+	} else if queryInfo.ActionType == 2 {
 		favoriteInfo.IsDeleted = UNLIKE
 	}
 
 	//存入Redis
-	ok := favoriteService.RedisAddFavorite(favoriteInfo)
+	//ok := favoriteService.RedisAddFavorite(favoriteInfo)
 
 	//如果存入成功返回
-	if ok == true {
-		c.JSON(-1, response.Response{http.StatusOK, "点赞失败"})
-	} else {
-		c.JSON(0, response.Response{http.StatusOK, "点赞成功"})
-	}
+	//if ok == true {
+	//	c.JSON(-1, response.Response{http.StatusOK, "点赞失败"})
+	//} else {
+	//	c.JSON(0, response.Response{http.StatusOK, "点赞成功"})
+	//}
+	c.JSON(0, response.Response{http.StatusOK, "点赞成功"})
 
 }
 
