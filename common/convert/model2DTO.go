b990e7518bb0947bb8c836ffe33bc40e4a6345c7
xlong99 <3375427@qq.com>
2022-05-27 20:04:16 +0800

    发布列表

---
+++ common/convert/model2DTO.go
@@ -0,0 +1,51 @@
+package convert
+
+import (
+	"github.com/DouYin/common/entity/dto"
+	"github.com/DouYin/common/model"
+)
+
+func User2UserDTO(user model.User) dto.UserDto {
+	return dto.UserDto{
+		Id:            user.UserId,
+		Name:          user.Name,
+		FollowCount:   user.FollowCount,
+		FollowerCount: user.FollowerCount,
+		IsFollow:      false,
+	}
+}
+func Video2VideoDto(video model.Video) dto.VideoDto {
+	var isFollow, isFavorite bool
+	if len(video.User.FollowedUser) != 0 {
+		isFollow = video.User.FollowedUser[0].IsDeleted
+	} else {
+		isFollow = false
+	}
+	if len(video.Favorite) != 0 {
+		isFavorite = video.Favorite[0].IsDeleted
+	} else {
+		isFavorite = false
+	}
+	return dto.VideoDto{
+		Id: video.VideoId,
+		Author: dto.UserDto{
+			Id:            video.User.UserId,
+			Name:          video.User.Username,
+			FollowCount:   video.User.FollowCount,
+			FollowerCount: video.User.FollowerCount,
+			IsFollow:      isFollow,
+		},
+		PlayURL:       video.Path,
+		CoverURL:      video.CoverPath,
+		FavoriteCount: video.FavoriteCount,
+		CommentCount:  video.CommentCount,
+		IsFavorite:    isFavorite,
+	}
+}
+func VideoList2VideoDtoList(videoList []model.Video) []dto.VideoDto {
+	var videoDtoList []dto.VideoDto
+	for i := range videoList {
+		videoDtoList = append(videoDtoList, Video2VideoDto(videoList[i]))
+	}
+	return videoDtoList
+}
--- douyinService/service/publishService.go
+++ douyinService/service/publishService.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"github.com/DouYin/common/context"
+	"github.com/DouYin/common/convert"
 	"github.com/DouYin/common/entity/dto"
 	"github.com/DouYin/common/model"
 	"github.com/DouYin/service/global"
@@ -43,31 +44,39 @@ func (ps *PublishService) Publish(userContext context.UserContext, data *multipa
 }
 
 func (ps *PublishService) PublishList(userContext context.UserContext, userId uint64) []dto.VideoDto {
-	//查询目标用户信息
-	userDtoInfo := userRepository.QueryUserDtoInfo(userId)
-	userDto := dto.UserDto{
-		Id:            userDtoInfo.UserId,
-		Name:          userDtoInfo.Name,
-		FollowCount:   userDtoInfo.FollowCount,
-		FollowerCount: userDtoInfo.FollowerCount,
-		IsFollow:      false,
-	}
-	//如果登录了，则填充IsFollow
-	if userContext.Id != 0 {
+
+	//查询目标用户信息,只查一遍
+	user := userRepository.QueryUserDtoInfo(userId)
+	userDto := convert.User2UserDTO(user)
+	var videoDtoList []dto.VideoDto
+	if userContext.Id == 0 {
+		videoDtoList = ps.publishListWithoutLogin(userId)
+	} else {
+		//如果登录了，则填充IsFollow
 		userDto.IsFollow = userRepository.IsFollow(userContext.Id, userId)
+		videoDtoList = ps.publishListWithLogin(userContext.Id, userId)
 	}
 
-	//获取用户上传的视频列表
-	videoList := videoRepository.GetPublishList(userId)
 	//返回结果
-	var videoDtoList []dto.VideoDto
-	for _, video := range videoList {
-		videoDtoList = append(videoDtoList, dto.VideoDto{
-			//给每个视频DTO填充Author信息
-			Author: userDto,
-			Id:     video.VideoId,
-		})
+	//给每个video填充作者信息
+	for i := range videoDtoList {
+		videoDtoList[i].Author = userDto
 	}
-	//TODO 填充is_favorite
+	return videoDtoList
+}
+func (ps *PublishService) publishListWithLogin(loginUser, userId uint64) []dto.VideoDto {
+	//获取用户上传的视频列表
+	videoList := videoRepository.GetPublishListWithFavorite(userId, 1, 30, loginUser)
+
+	videoDtoList := convert.VideoList2VideoDtoList(videoList)
+
+	return videoDtoList
+}
+func (ps *PublishService) publishListWithoutLogin(userId uint64) []dto.VideoDto {
+	//获取用户上传的视频列表
+	videoList := videoRepository.GetPublishList(userId, 1, 30)
+
+	videoDtoList := convert.VideoList2VideoDtoList(videoList)
+
 	return videoDtoList
 }
