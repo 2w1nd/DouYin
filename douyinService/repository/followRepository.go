c6ae07ad679038266fa258d9a265fbee658346fe
bnnpuu <75206829+bnnpuu@users.noreply.github.com>
2022-06-09 20:54:12 +0800

    #改变粉丝关注数据库修改规则

--- douyinService/repository/followRepository.go
+++ douyinService/repository/followRepository.go
@@ -1,102 +1,101 @@
 package repository
 
 import (
-	"github.com/DouYin/common/codes"
-	"github.com/DouYin/common/entity/dto"
 	"github.com/DouYin/common/model"
 	"github.com/DouYin/service/global"
-	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
 )
 
-type FollowRepository struct {
+type UserRepository struct {
 	Base BaseRepository
-	User UserRepository
 }
 
-func (r *FollowRepository) AddFollow(follow model.Follow) bool {
-	where := model.User{
-		UserId: follow.FollowedUserId,
-	}
-	if _, err := r.User.GetFirstUser(where); err != nil {
+// QueryUserDtoInfo 查询UserDto需要的信息
+func (u UserRepository) QueryUserDtoInfo(userId uint64) model.User {
+	var user model.User
+	query := global.DB.Debug().
+		Model(model.User{})
+	query.Select("id", "user_id", "name", "follow_count", "follower_count").
+		Where("user_id = ?", userId).Limit(1)
+	query.Find(&user)
+	return user
+}
+
+func (u UserRepository) UpdateFollowCount(userId uint64, followCount uint32) bool {
+	var user model.User
+	query := global.DB.Debug().
+		Model(model.User{})
+	query.Select("id", "user_id", "name", "follow_count", "follower_count").
+		Where("user_id = ?", userId).Limit(1)
+	query.Find(&user)
+	db := global.DB.Where(user)
+	var out model.User
+	if err := db.Model(out).Debug().Where(user).Update("follow_count", followCount).Error; err != nil {
 		return false
 	}
-	if err := r.Base.Create(&follow); err != nil {
-		return false
-	}
+
 	return true
 }
 
-func (r *FollowRepository) UpdateFollowUserId(where model.Follow, out interface{}) bool {
-	if err := global.DB.Debug().Clauses(clause.OnConflict{
-		Columns:   []clause.Column{{Name: "user_id"}, {Name: "followed_user_id"}},
-		DoUpdates: clause.AssignmentColumns([]string{"is_deleted"}),
-	}).Create(&where); err != nil {
-		return false
-	}
-	return true
-	//db := global.DB.Where(where)
-	//if err := db.Model(out).Where(where).Update("is_deleted", 0).RowsAffected; err == 0 {
-	//	return false
-	//}
-	//return true
-}
+func (u UserRepository) UpdateFollowerCount(userId uint64, followerCount uint32) bool {
+	var user model.User
+	query := global.DB.Debug().
+		Model(model.User{})
+	query.Select("id", "user_id", "name", "follow_count", "follower_count").
+		Where("user_id = ?", userId).Limit(1)
+	query.Find(&user)
+	db := global.DB.Where(user)
+	var out model.User
 
-func (r *FollowRepository) DeleteFollowUserId(where interface{}) bool {
-	var follow model.Follow
-	if err := r.Base.DeleteSoftByID(where, &follow); err != nil {
+	if err := db.Model(out).Debug().Where(user).Update("follower_count", followerCount).Error; err != nil {
 		return false
 	}
+
 	return true
 }
 
-func (r *FollowRepository) GetFollowerListByUserId(id uint64) ([]model.Follow, error) {
-	var followList []model.Follow
-	query := global.DB.
-		Model(model.Follow{}).
-		Where("followed_user_id = ?", id).
-		Preload("User")
-	err := query.Debug().Find(&followList).Error
+// IsFollow 判断关注关系
+// src 源 dst 目标
+func (u UserRepository) IsFollow(src, dst uint64) bool {
+	var count int64
+	query := global.DB.Debug().
+		Model(model.Follow{})
+	query.
+		Where("user_id = ? and followed_user_id = ? and is_deleted = 0", src, dst).Limit(1)
+	query.Count(&count)
+	return count > 0
+}
+
+func (u UserRepository) CreateUser(user *model.User) (*model.User, error) {
+	err := global.DB.Raw("INSERT INTO douyin_user(user_id,name,username,password,salt) VALUES(?,?,?,?,?);",
+		user.UserId, user.Name, user.Username, user.Password, user.Salt).Scan(&user).Error
 	if err != nil {
-		return followList, err
+		return nil, err
 	}
-	return followList, nil
+	return user, nil
 }
 
-// GetFollowedOrFollowUserWithUserId 获得关注列表或粉丝列表，Followed是粉丝列表，Follow是关注列表
-func (r *FollowRepository) GetFollowedOrFollowUserWithUserId(id uint64, Type int) ([]dto.FollowDto, error) {
-	var followedList []dto.FollowDto
-
-	var subQuery, subQuery1 *gorm.DB
-	var sqlQ string
-	subQuery = global.DB.Model(model.Follow{}).Select("user_id", "followed_user_id", "is_deleted")
-	subQuery1 = global.DB.Model(model.Follow{}).Select("user_id", "followed_user_id", "is_deleted")
-	if Type == codes.Followed {
-		subQuery = subQuery.Where("followed_user_id = ?", id)
-		subQuery1 = subQuery1.Where("user_id = ?", id)
-		sqlQ = "LEFT JOIN douyin_user on a.user_id = douyin_user.user_id"
-	} else if Type == codes.Follow {
-		subQuery = subQuery.Where("user_id = ?", id)
-		subQuery1 = subQuery1.Where("followed_user_id = ?", id)
-		sqlQ = "LEFT JOIN douyin_user on a.followed_user_id = douyin_user.user_id"
+func (u *UserRepository) GetFirstUser(where interface{}) (model.User, error) {
+	var user model.User
+	if err := u.Base.First(where, &user); err != nil {
+		return model.User{}, err
 	}
+	return user, nil
+}
 
-	err := global.DB.Debug().
-		Select("a.user_id ",
-			"username as name",
-			"a.followed_user_id ",
-			"CASE WHEN a.is_deleted = 0 THEN true ELSE false END as followed_a ",
-			"CASE WHEN b.is_deleted = 0 THEN true ELSE false END as followed_b ",
-			"follow_count ",
-			"follower_count ").
-		Where("a.is_deleted = 0").
-		Table("(?) as a "+
-			"LEFT JOIN (?) as b "+
-			"ON a.user_id = b.followed_user_id AND a.followed_user_id = b.user_id "+sqlQ, subQuery, subQuery1).
-		Find(&followedList).Error
-
+func (u UserRepository) GetUserByUserName(username string) (model.User, error) {
+	user := model.User{}
+	err := global.DB.Where("username = ?", username).First(&user).Error
 	if err != nil {
-		return followedList, err
+		return model.User{}, err
 	}
-	return followedList, nil
+	return user, nil
+}
+
+func (u UserRepository) GetFollowByUserId(userId, followerId uint64) (model.Follow, error) {
+	follow := model.Follow{}
+	err := global.DB.Where("user_id = ? AND followed_user_id = ?", userId, followerId).First(&follow).Error
+	if err != nil {
+		return model.Follow{}, err
+	}
+	return follow, nil
 }
